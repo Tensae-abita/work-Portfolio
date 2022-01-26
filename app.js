4d8eec3e364a84423fab77d165a6cd9c4a0e6a8b
tensu <tensuabita0@gmail.com>
2022-01-26 22:01:48 +0300

    finished

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+
+app.use(express.static("public"));
+
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/index.html");
+});
+
+app.listen(8080 || process.env.PORT, function (req, res) {
+  console.log("app running on port 8080");
+});
