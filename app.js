21b1eb6124a9961151206ae78dac8e04bae51996
tensu <tensuabita0@gmail.com>
2022-02-16 22:33:27 +0300

    added auto date footer

--- app.js
+++ app.js
@@ -7,6 +7,6 @@ app.get("/", function (req, res) {
   res.sendFile(__dirname + "/index.html");
 });
 
-app.listen(process.env.PORT, function (req, res) {
+app.listen(process.env.PORT || 8080, function (req, res) {
   console.log("app running on port 8080");
 });
